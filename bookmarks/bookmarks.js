0fecd9c4f3747a0501f448c30ce846c713daa7a3
voyagesj <llsjleell@outlook.com>
2024-12-18 18:09:22 +0900

    add bookmarks

---
+++ bookmarks/bookmarks.js
@@ -0,0 +1,44 @@
+var linkJson = {
+	"Neoplus": {
+		"portal.neosystem.net 비밀번호 변경": "http://portal.neosystem.net/cmnmgt/password/resetpassword.aspx?MenuID=X1000107",
+		"현대중공업원격접속": "https://nc.hhi.co.kr/nc20/intro.jsp",
+		"Neoplus Devops": "http://devops.neosystem.net/Projects"
+	},
+	"미분류": {
+		"GitBlog":"https://github.com/voyagesj/Blog/tree/master/posts",
+		"Intersection Observer API로 무한 스크롤 구현하기": "https://nohack.tistory.com/124",
+		"NuGet Gallery": "https://www.nuget.org/",
+		"Chat GPT": "https://chat.openai.com/chat",
+		"Guide - AutoSPInstaller Online": "https://autospinstaller.com/Guide",
+		"SharePoint 2019 – TCP/IP Ports - SharePointTechnicalSupport": "https://sharepointtechnicalsupport.com/sharepoint-2019-tcp-ip-ports/",
+		"AD FS 2019를 사용하여 HTTP 보안 응답 헤더 사용자 지정": "https://learn.microsoft.com/en-us/windows-server/identity/ad-fs/operations/customize-http-security-headers-ad-fs",
+		"SQL 서버를 이용한 ASP.NET 세션관리": "https://sourcenote.tistory.com/8",
+		"방화벽확인 netstat -an | find 80": "https://m.blog.naver.com/PostView.nhn?blogId=gungrave&logNo=120065216319&proxyReferer=https:%2F%2Fwww.google.com%2F",
+		"웹사이트 동작 체크 명령어 - cURL": "https://hyeo-noo.tistory.com/387",
+		"cURL":"https://everything.curl.dev/project/does.html"
+	},
+	"M365": {
+		"Graph Explorer | Try Microsoft Graph APIs - Microsoft Graph": "https://developer.microsoft.com/en-us/graph/graph-explorer",
+		"Microsoft Learn | TEAMS": "https://learn.microsoft.com/ko-kr/training/modules/explore-teams/4-integration-microsoft-power-platform",
+		"Authenticate and authorize with the Office dialog API - Office Add-ins | Microsoft Learn": "https://learn.microsoft.com/en-us/office/dev/add-ins/develop/auth-with-office-dialog-api",
+		"Register your tab app with Azure AD - Teams | Microsoft Learn": "https://learn.microsoft.com/ko-kr/microsoftteams/platform/tabs/how-to/authentication/tab-sso-register-aad",
+		"Find Azure AD PowerShell and MSOnline cmdlets in Microsoft Graph PowerShell": "https://learn.microsoft.com/en-us/powershell/microsoftgraph/azuread-msoline-cmdlet-map?view=graph-powershell-1.0&pivots=azure-ad-powershell",
+		"Teamsapp validator": "https://dev.teams.microsoft.com/appvalidation.html",
+		"AdaptiveCard Schema Explorer": "https://adaptivecards.io/explorer/AdaptiveCard.html",
+		"conditional access": "https://learn.microsoft.com/ko-kr/entra/identity/conditional-access/overview",
+		"Conditional Access app control in Microsoft Defender for Cloud Apps": "https://learn.microsoft.com/en-us/defender-cloud-apps/proxy-intro-aad",
+		"Microsoft Intune": "https://learn.microsoft.com/ko-kr/mem/intune/fundamentals/what-is-intune",
+		"PowerShell 실행 정책설정": "https://learn.microsoft.com/ko-kr/powershell/module/microsoft.powershell.core/about/about_execution_policies?view=powershell-7.4"
+	},
+	"Outlook Addin": {
+		"Deploy add-ins in the Microsoft 365 admin center": "https://learn.microsoft.com/ko-kr/microsoft-365/admin/manage/manage-deployment-of-add-ins?view=o365-worldwide",
+		"Office.MessageRead interface": "https://learn.microsoft.com/en-us/javascript/api/outlook/office.messageread?view=outlook-js-preview",
+		"Graph GetMessage": "https://learn.microsoft.com/en-us/graph/api/message-get?view=graph-rest-1.0&tabs=csharp#example-4-get-mime-content",
+		"ExtensionPoint element": "https://learn.microsoft.com/en-us/javascript/api/manifest/extensionpoint?view=common-js-preview",
+		"Clear the Office cache": "https://learn.microsoft.com/en-us/office/dev/add-ins/testing/clear-cache"
+	},
+	"Copilot": {
+		"Your extensibility options for Microsoft 365 Copilot": "https://learn.microsoft.com/en-us/microsoft-365-copilot/extensibility/decision-guide",
+		"How Microsoft 365 Copilot can work with your external data": "https://techcommunity.microsoft.com/blog/microsoftmechanicsblog/how-microsoft-365-copilot-can-work-with-your-external-data/3937645"
+	}
+};
